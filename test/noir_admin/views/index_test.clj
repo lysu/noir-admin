5ee93d4e263f2d0973e8415990d300d24fca23f4
lysu <sulifx@gmail.com>
2012-07-15 13:54:48 +0800

    can't test defpage?

--- test/noir_admin/views/index_test.clj
+++ test/noir_admin/views/index_test.clj
@@ -1,7 +1,3 @@
 (ns noir-admin.views.index-test
   (:use clojure.test
     noir-admin.views.index))
-
-; (deftest should-show-index-page-test
-;   (testing "show index page"
-;     (is (.contains (index) "Admin"))))
