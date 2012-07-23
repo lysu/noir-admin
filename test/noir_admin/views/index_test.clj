99d3497e454b6466978624672e455c1f67a6c7a7
lysu <sulifx@gmail.com>
2012-07-23 23:49:44 +0800

    defpage cound test too

--- test/noir_admin/views/index_test.clj
+++ test/noir_admin/views/index_test.clj
@@ -1,3 +1,8 @@
 (ns noir-admin.views.index-test
-  (:use clojure.test
-    noir-admin.views.index))
+  (:use [clojure.test]
+        [noir-admin.views.index]
+        [noir.util.test]))
+
+(deftest route-test
+  (-> (send-request "/admin/" {})
+      (has-status 200)))
